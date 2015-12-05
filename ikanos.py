0f59f4a8325f46ec7a26cbf8a35bad39a9e4bb8f
thyagostall <thstall@gmail.com>
2015-12-05 18:15:01 -0200

    Thread logic completed

--- ikanos.py
+++ ikanos.py
@@ -3,68 +3,101 @@ import socket
 import time
 import os
 
+HOST = ''
+PORT = 8888
+BASE_DIR = 'www/'
+
+def log(type, message):
+    print(type + ": " + message)
+
 class ClientThread(threading.Thread):
-    base_dir = "./www/"
-
-    def file_to_bytes(self, file_name):
-        complete_file_name = os.path.dirname(os.path.realpath(__file__))
-        complete_file_name = os.path.xjoin(complete_file_name, self.base_dir, file_name)
-
-        myfile = open(complete_file_name, "r")
-        data = ""
-        lines = myfile.readlines()
-
-        for line in lines:
-            data = data + line.strip();
-
-        data = bytes(data, 'UTF-8')
-        return data
-
     def bytes_from_file(self, filename, chunksize=8192):
         result = []
-        with open(filename, "rb") as f:
-            while True:
-                chunk = f.read(chunksize)
-                if chunk:
-                    result.append(chunk)
-                else:
-                    break
+
+        try:
+            with open(filename, "rb") as f:
+                while True:
+                    chunk = f.read(chunksize)
+                    if chunk:
+                        result.append(chunk)
+                    else:
+                        break
+        except:
+            result = []
+
         return result
 
-    def get_file_name(self, request):
+    def request_to_filename(self, request):
         request_string = request.decode('UTF-8')
-
         start = 5
         end = request_string.find("HTTP/1.") - 1
 
-        file_name = os.path.dirname(os.path.realpath(__file__))
-        return os.path.join(file_name, self.base_dir, request_string[start:end])
+        return request_string[start:end]
+
+    def get_filename(self, request_file_name):
+        if not request_file_name:
+            request_file_name = 'index.html'
+
+        if not self.get_file_ext(request_file_name):
+            request_file_name += '.html'
+
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), BASE_DIR, request_file_name)
+
+    def get_file_ext(self, file_name):
+        extension = os.path.splitext(file_name)
+        return extension[len(extension) - 1]
 
     def __init__(self, client_connection):
          self.client_connection = client_connection
          super(ClientThread, self).__init__()
 
+    def get_header(self, response_code):
+        result = "HTTP/1.0 " + str(response_code)
+
+        if response_code == 200:
+            result += " OK"
+        else:
+            result += " Not Found"
+
+        result += "\n\n"
+        return result
+
     def run(self):
-        request = self.client_connection.recv(1024)
+        try:
+            request = self.client_connection.recv(1024)
 
-        http_response = """\
-HTTP/1.0 200 OK
+            file_name = self.request_to_filename(request)
+            file_name = self.get_filename(file_name)
 
-"""
-        data = self.bytes_from_file(self.get_file_name(request))
+            log('Debug', "Request for: " + file_name)
 
-        self.client_connection.sendall(bytes(http_response, 'UTF-8'))
+            response = self.bytes_from_file(file_name)
 
-        for i in data:
-            self.client_connection.sendall(i)
+            header = ""
+            if response:
+                header = self.get_header(200)
+            else:
+                header = self.get_header(404)
+                response = self.bytes_from_file(self.get_filename("404.html"))
+
+            log('Debug', "Response header: '" + header.strip() + "'")
+        except:
+            header = self.get_header(500)
+            response = self.bytes_from_file(self.get_filename("500.html"))
+
+        try:
+            self.client_connection.sendall(bytes(header, 'UTF-8'))
+            for i in response:
+                self.client_connection.sendall(i)
+        except:
+            pass
 
         self.client_connection.close()
+        log('Debug', 'Socket closed')
 
 
 class Server:
     def serve(self):
-        HOST, PORT = '', 8888
-
         listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listen_socket.bind((HOST, PORT))
@@ -73,23 +106,9 @@ class Server:
 
         while True:
             (clientsocket, address) = listen_socket.accept()
+            log('Debug', 'New socket: ' + str(address))
             client = ClientThread(clientsocket)
             client.start()
-
-# import socket
-#
-# while True:
-#     client_connection, client_address = listen_socket.accept()
-#     request = client_connection.recv(1024)
-#     # print(request)
-#
-#     http_response = """\
-# HTTP/1.0 200 OK
-#
-# Hello, World!
-# """
-#     client_connection.sendall(bytes(http_response, 'UTF-8'))
-#     client_connection.close()
 
 if __name__ == "__main__":
     Server().serve()
